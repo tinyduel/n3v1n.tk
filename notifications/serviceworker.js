79ccd9de0faebf9bddbf1d34573c601ca5fb1939
tinyduel <46302335+tinyduel@users.noreply.github.com>
2019-01-08 10:35:18 -0500

    Create serviceworker.js

---
+++ notifications/serviceworker.js
@@ -0,0 +1,18 @@
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('http://www.example.com');
+    notification.close();
+  }
+});
